efd88608af4e9fe209fe19a7aee5721fec72513d
sbrener1610 <sbrener1610@gmail.com>
2023-06-20 20:08:33 +0200

    Sorting algorithm implemented

--- Sergey.py
+++ Sergey.py
@@ -4,19 +4,17 @@ from termcolor import colored
 
 
 
-SkalaRange=5
-Preferences=[1,1,1]
-Importance=[1,1,1]  
-Cur=0
 
-def DrawSkala(i,bool):
+
+def DrawSkala(i,bool):   # Draws a scale menu of the type <......X..........>
+    SkalaRange=5
     Skala=""
     Skala+="<"+(i-1)*"....."+"..X.."+(SkalaRange-i)*"....."+">"
     if bool:
-        Skala+=colored("  █",'red')
+        Skala+=colored("  █",'red')    #Puts a mark at the currently active menu
     return Skala
 
-def PrintField(Cur):
+def PrintField(Cur,Preferences,Importance):    # Prints all six scale menus
     x("clear")
     print("Use s, w to navigate; a, d to change the input, space to submit.\n\n")
     print("Prefered climate (1 coldest, 5 hottest):\n"+DrawSkala(Preferences[0],Cur==0)+"\n   1    2    3    4    5\n")
@@ -26,26 +24,104 @@ def PrintField(Cur):
     print("Prefered safety (1 war zone, 5 safest):\n"+DrawSkala(Preferences[2],Cur==4)+"\n   1    2    3    4    5\n")
     print("Imortance of safety (1 unimportant, 5 very important):\n"+DrawSkala(Importance[2],Cur==5)+"\n   1    2    3    4    5\n")
 
+def RunPromt():     # User changes his preferences
+    Preferences=[3,3,3]
+    Importance=[1,1,1]  
+    Cur=0
+    while True:
+        PrintField(Cur,Preferences,Importance)
+        Move=getch.getch()
+        if Move=="w" and Cur>0:   # Navigating the menus
+            Cur-=1
+        if Move=="s" and Cur<5:
+            Cur+=1
+        if Move=="a" and Cur%2==0 and Preferences[Cur//2]>1:    # Changing the value at the current menu
+            Preferences[Cur//2]-=1
+        if Move=="a" and Cur%2==1 and Importance[Cur//2]>1:
+            Importance[Cur//2]-=1
+        if Move=="d" and Cur%2==0 and Preferences[Cur//2]<5:
+            Preferences[Cur//2]+=1
+        if Move=="d" and Cur%2==1 and Importance[Cur//2]<5:
+            Importance[Cur//2]+=1
+        if Move==" ":    # Select and return chosen values
+            break
 
-while True:
-    PrintField(Cur)
-    Move=getch.getch()
-    if Move=="w" and Cur>0:
-        Cur-=1
-    if Move=="s" and Cur<5:
-        Cur+=1
-    if Move=="a" and Cur%2==0 and Preferences[Cur//2]>1:
-        Preferences[Cur//2]-=1
-    if Move=="a" and Cur%2==1 and Importance[Cur//2]>1:
-        Importance[Cur//2]-=1
-    if Move=="d" and Cur%2==0 and Preferences[Cur//2]<5:
-        Preferences[Cur//2]+=1
-    if Move=="d" and Cur%2==1 and Importance[Cur//2]<5:
-        Importance[Cur//2]+=1
-    if Move==" ":
-        break
+    return [Preferences,Importance]
 
-print(Preferences,Importance)
+def AssignWeightedValues(ListOfPref, database):    # Calculates the absolute value of the discrepancy from the desired value of a parameter and the value of that parameter for a given destination. Multiplies the result with the user-selected importance (importance 1 means multiplication by 0)
+    WeightedList=[]
+    InfoKeys=["Climate","Price","Safety"]
+    for x in database:
+        Discrepancy=0
+        for y in InfoKeys:
+            Discrepancy+=abs(ListOfPref[0][InfoKeys.index(y)]-database[x][y])*(ListOfPref[1][InfoKeys.index(y)]-1)
+        WeightedList.append([x,Discrepancy])
+    return WeightedList   
+
+def ReturnWeight(item):
+    return item[1]
+
+def OrderWeightedList(WeightedList):   #Sorts the destination according to AssignedWeightedValues
+    WeightedList.sort(key=ReturnWeight)
+    Chop=3
+    while WeightedList[Chop-1][1]==WeightedList[Chop][1]:    #If there's a tie between 3rd and 4th place includes all places after the 3rd one that are tied
+        Chop+=1
+        if Chop==len(WeightedList):
+            break
+    WeightedListStrippedChopped=[]
+    for i in range(Chop):
+        WeightedListStrippedChopped.append(WeightedList[i][0])
+    return WeightedListStrippedChopped
+
+TopLevel = [[["Info","i"],"Input","Random"],[["Choose","c"],"Input","Random"],[["apPend","p"]],[["eXit","x"]]]
+
+def TopMenu(i,N,W):
+    k=1
+    middle=""
+    while k<=N:
+        if k!=i:
+           middle+="│"+TopLevel[k-1][0][0].center(W)
+        else: 
+           middle+="│"
+           middle+=colored(TopLevel[k-1][0][0].center(W),'green') 
+        k+=1
+    middle+="│\n" 
+    return middle    #  highlighted top menu item
+
+def TopMenuBottomOpen(i,N,W):
+    if i==1:
+        bottomOpen="├"+"─"*W+"┼"+"─"*W+("┴"+"─"*W)*(N-2)+"┘\n"
+    elif i==N:
+        bottomOpen="└"+"─"*W+("┴"+"─"*W)*(N-2)+"┼"+"─"*W+"┤\n"
+    else:
+        bottomOpen="└"+"─"*W+("┴"+"─"*W)*(i-2)+"┼"+"─"*W+"┼"+("─"*W+"┴")*(N-i-1)+"─"*W+"┘\n"
+    return bottomOpen      #  bottom of menu open at i-th position
+
+
+def DropOutMenuOpen(i,j,W):    #Dropout menu at i-th horizontal position highlighted at j-th vertical position
+    DropOutMenu=""
+    k=1
+    while k<len(TopLevel[i-1]):
+        if j==k+1:
+            DropOutMenu+=" "*(W+1)*(i-1)+"│"+colored(TopLevel[i-1][k].center(W),'blue')+"│\n"
+        else:
+            DropOutMenu+=" "*(W+1)*(i-1)+"│"+TopLevel[i-1][k].center(W)+"│\n"
+        k+=1
+    DropOutMenu+=" "*(W+1)*(i-1)+"└"+"─"*W+"┘\n"
+    return DropOutMenu
+
+def PrintMenu(i,j,N,W):     #prints the menu 
+    top="┌"+"─"*W+("┬"+"─"*W)*(N-2)+"┬"+"─"*W+"┐\n"  # top of menu
+    bottom="└"+"─"*W+("┴"+"─"*W)*(N-1)+"┘\n"      # bottom of closed menu
+    middle = TopMenu(i,N,W)
+    x("clear")
+    print("a,s,d,w or shortcuts to navigate, space to select")
+    if j==1:    # closed menu
+        print(top+middle+bottom)
+    else:       # menu open at i-th position
+        bottomOpen=TopMenuBottomOpen(i,N,W)
+        DropOutMenu=DropOutMenuOpen(i,j,W)
+        print(top+middle+bottomOpen+DropOutMenu)
 
 
 
--- main.py
+++ main.py
@@ -5,20 +5,102 @@ from termcolor import colored
 import getch
 import Sergey
 
+database = {
+    "Germany": {
+        "Information": "Rich history, vibrant cities, and a cool climate",
+        "Climate": 2,
+        "Price": 3,
+        "Safety": 4
+    },
+    "Japan": {
+        "Information": "A blend of tradition and modernity with pleasant temperatures",
+        "Climate": 4,
+        "Price": 3,
+        "Safety": 4
+    },
+    "Japan1": {
+        "Information": "A blend of tradition and modernity with pleasant temperatures",
+        "Climate": 1,
+        "Price": 1,
+        "Safety": 5
+    },
+    "Japan2": {
+        "Information": "A blend of tradition and modernity with pleasant temperatures",
+        "Climate": 2,
+        "Price": 5,
+        "Safety": 3
+    },
+    "Japan3": {
+        "Information": "A blend of tradition and modernity with pleasant temperatures",
+        "Climate": 2,
+        "Price": 1,
+        "Safety": 1
+    },
+    "Japan4": {
+        "Information": "A blend of tradition and modernity with pleasant temperatures",
+        "Climate": 5,
+        "Price": 5,
+        "Safety": 2
+    },
+    "Japan5": {
+        "Information": "A blend of tradition and modernity with pleasant temperatures",
+        "Climate": 2,
+        "Price": 5,
+        "Safety": 1
+    },
+    "Japan6": {
+        "Information": "A blend of tradition and modernity with pleasant temperatures",
+        "Climate": 2,
+        "Price": 3,
+        "Safety": 1
+    },
+    "Japan7": {
+        "Information": "A blend of tradition and modernity with pleasant temperatures",
+        "Climate": 1,
+        "Price": 2,
+        "Safety": 5
+    },
+    "Japan8": {
+        "Information": "A blend of tradition and modernity with pleasant temperatures",
+        "Climate": 2,
+        "Price": 1,
+        "Safety": 3
+    },
+    "Japan9": {
+        "Information": "A blend of tradition and modernity with pleasant temperatures",
+        "Climate": 3,
+        "Price": 4,
+        "Safety": 2
+    },
+    "Japan10": {
+        "Information": "A blend of tradition and modernity with pleasant temperatures",
+        "Climate": 5,
+        "Price": 1,
+        "Safety": 1
+    }
+    # Ill add the information for the other 22 countries here
+}
+
+def CreateAllDestinations(dict):
+    return  list(dict.keys())
+
+AllDestinations = CreateAllDestinations(database)
+
 N=4 # number of menu items
 W=8 # width of menu item
-TopLevel = [[["Info","i"],"Input","Random"],[["Choose","c"],"Input","Random"],[["apPend","p"]],[["eXit","x"]]]
+#TopLevel = [[["Info","i"],"Input","Random"],[["Choose","c"],"Input","Random"],[["apPend","p"]],[["eXit","x"]]]
 
 ShortCut=[]
 p=0
-while p<len(TopLevel):    # creates a list of keyboard shortcuts
-    ShortCut.append(TopLevel[p][0][1])
+while p<len(Sergey.TopLevel):    # creates a list of keyboard shortcuts
+    ShortCut.append(Sergey.TopLevel[p][0][1])
     p+=1
 
 # Elements of the menu
-
+"""
 top="┌"+"─"*W+("┬"+"─"*W)*(N-2)+"┬"+"─"*W+"┐\n"  # top of menu
 bottom="└"+"─"*W+("┴"+"─"*W)*(N-1)+"┘\n"      # bottom of closed menu
+
 
 def TopMenu(i):
     k=1
@@ -55,6 +137,7 @@ def DropOutMenuOpen(i,j):
     DropOutMenu+=" "*(W+1)*(i-1)+"└"+"─"*W+"┘\n"
     return DropOutMenu
 
+
 def PrintMenu(i,j):
     middle = TopMenu(i)
     x("clear")
@@ -65,14 +148,19 @@ def PrintMenu(i,j):
         bottomOpen=TopMenuBottomOpen(i)
         DropOutMenu=DropOutMenuOpen(i,j)
         print(top+middle+bottomOpen+DropOutMenu)
-
+"""
 def Execute(i,j):
     if i==1 and j==2:
         input("Enter a countryname")
     elif i==1 and j==3:
         input("Here is a random country")
     elif i==2 and j==2:
-        input("What are your preferences")
+        ListOfPref=Sergey.RunPromt()
+        WeightedList=Sergey.AssignWeightedValues(ListOfPref,database)
+        print(WeightedList)
+        print(Sergey.OrderWeightedList(WeightedList))
+
+        getch.getch()
     elif i==2 and j==3:
         input("Enter your preferences and get a surprise")
     elif i==3 and j==1:
@@ -84,28 +172,28 @@ def Execute(i,j):
 i=1
 j=1
 x("clear")
-PrintMenu(i,j)
+Sergey.PrintMenu(i,j,N,W)
 while True:
     Move=getch.getch().lower()
     if Move=="a":
         if i>1:
             i-=1
-            if j>len(TopLevel[i-1]):   #checks the size of the dropout menu at the new position. If too large - adjust.
-                j=len(TopLevel[i-1])
+            if j>len(Sergey.TopLevel[i-1]):   #checks the size of the dropout menu at the new position. If too large - adjust.
+                j=len(Sergey.TopLevel[i-1])
     if Move=="d":
         if i<N:
             i+=1
-            if j>len(TopLevel[i-1]):
-                j=len(TopLevel[i-1])
+            if j>len(Sergey.TopLevel[i-1]):
+                j=len(Sergey.TopLevel[i-1])
     if Move=="s":
-        if j<len(TopLevel[i-1]):
+        if j<len(Sergey.TopLevel[i-1]):
            j+=1
     if Move=="w":
         if j>1:
            j-=1
     if Move in ShortCut:     # if shortcut is used, moves to the corresponding menu item and opens menu
         i=ShortCut.index(Move)+1
-        if len(TopLevel[i-1])>1:
+        if len(Sergey.TopLevel[i-1])>1:
             j=2
         else:
             j=1
@@ -116,13 +204,13 @@ while True:
         i=1
         j=1
     elif Move==" " and j==1:     #if submenu not empty space opens it, otherwise executes command
-        if len(TopLevel[i-1])>1:
+        if len(Sergey.TopLevel[i-1])>1:
             j=2
         else:
             Execute(i,j)
             i=1
             j=1
-    PrintMenu(i,j)
+    Sergey.PrintMenu(i,j,N,W)
     if Move=="x":
         break
 
